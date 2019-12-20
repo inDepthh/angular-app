7d0b7e0b445b3c552a79473446ffcfdc8a27a6e1
Ryan Couture <rcouture@picapd.com>
2019-12-20 13:25:08 -0500

    add dynamic list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,3 @@
-// import { BrowserModule } from '@angular/platform-browser';
-// import { NgModule } from '@angular/core';
-// import { AuthGuard } from './auth.guard';
-// import { RouterModule, Routes } from '@angular/router';
-// import { AppRoutingModule } from './app-routing.module';
-// import { AppComponent } from './app.component';
-// import { LoginComponent } from './login/login.component';
-// import { DashboardComponent } from './dashboard/dashboard.component';
-
-// const appRoutes: Routes = [
-//   { path: 'login', component: LoginComponent },
-//   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] }
-// ];
-
-// @NgModule({
-//   declarations: [
-//     AppComponent,
-//     LoginComponent,
-//     DashboardComponent
-//   ],
-//   imports: [
-//     BrowserModule,
-//     AppRoutingModule
-//   ],
-//   providers: [AuthGuard],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
-
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
 
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -10,6 +11,8 @@ import { Router } from '@angular/router';
 export class DashboardComponent implements OnInit {
 
   id: string;
+  items = ["item1", "item2"];
+
   constructor(private router: Router,public authService: AuthService) { }
 
   ngOnInit() {
@@ -20,5 +23,13 @@ export class DashboardComponent implements OnInit {
     console.log("Logout"); 
     this.authService.logout();
     this.router.navigate(['/login']);
+  }
+
+  addItems(item) {
+    this.items.push(item)
+  }
+
+  removeItems(index: number) {
+    this.items.splice(index, 1)
   }
 }
